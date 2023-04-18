6b64190c2fb223b4b1d1189f277ec92694e1db22
Francisco V. Diaz <fevarga1@uc.cl>
2023-04-18 01:22:38 -0400

    created product manager CRUD with data file creation. missing todo Update and Delete Methods.

--- dist/ProductManager.js
+++ dist/ProductManager.js
@@ -1,10 +1,40 @@
 "use strict";
+const fs = require("fs");
 class ProductManager {
-    constructor() {
+    constructor(path) {
+        this.path = path;
         this.products = [];
         this.currentId = 0;
     }
-    addProduct(product) {
+    async loadData() {
+        try {
+            const data = await fs.promises.readFile(this.path, "utf-8");
+            const parsedData = JSON.parse(data);
+            this.products = parsedData.products;
+            this.currentId = parsedData.lastId;
+            console.log(this.currentId);
+        }
+        catch (error) {
+            console.log("Error loading data!");
+            console.log("Creating new data file...");
+            try {
+                await fs.promises.writeFile(this.path, JSON.stringify({ lastId: 0, products: [] }, null, 2), 'utf-8');
+                console.log("Data file created");
+            }
+            catch (error) {
+                console.log("Error creating data file!");
+            }
+        }
+    }
+    async saveData() {
+        try {
+            await fs.promises.writeFile(this.path, JSON.stringify({ lastId: this.currentId, products: this.products }, null, 2), 'utf-8');
+        }
+        catch (error) {
+            console.log("Error saving data!");
+        }
+    }
+    async addProduct(product) {
         // Check if product already exists
         if (this.products.some((item) => item.code === product.code)) {
             console.log("Product already exists");
@@ -21,8 +51,9 @@ class ProductManager {
             return;
         }
         // Else, add product
-        product = { id: this.currentId++, ...product };
+        product = { id: ++this.currentId, ...product };
         this.products.push(product);
+        await this.saveData();
     }
     getProductById(id) {
         // Check if product exists
@@ -33,41 +64,58 @@ class ProductManager {
     }
 }
 // TESTS //
+test();
+async function test() {
+    // Create product manager instance
+    const myProductManager = new ProductManager("data.json");
+    // Load data.json file if exists. Else, it creates a new data.json file
+    await myProductManager.loadData();
+    // Show current products
+    console.log(myProductManager.getProducts());
+    // Created product1
+    const product1 = {
+        title: "producto prueba",
+        description: "Este es un producto prueba 1",
+        price: 200,
+        thumbnail: "Sin imagen",
+        code: "abc123",
+        stock: 25,
+    };
+    // Add product and await to save changes in data file.
+    await myProductManager.addProduct(product1);
+    // Show current products
+    console.log(myProductManager.getProducts());
+    // Check product by ID
+    console.log(myProductManager.getProductById(0));
+    const product2 = {
+        title: "producto prueba 2",
+        description: "Este es un producto prueba 2",
+        price: 200,
+        thumbnail: "Sin imagen",
+        code: "abc321",
+        stock: 25,
+    };
+    myProductManager.addProduct(product2);
+    console.log(myProductManager.getProducts());
+    console.log(myProductManager.getProductById(1));
+}
 // Create product manager
-const myProductManager = new ProductManager();
+// const myProductManager = new ProductManager("data.json");
 // Show current products. Should be empty
-console.log(myProductManager.getProducts());
-// Create product 1
-const product1 = {
-    title: "producto prueba",
-    description: "Este es un producto prueba 1",
-    price: 200,
-    thumbnail: "Sin imagen",
-    code: "abc123",
-    stock: 25,
-};
+// console.log(myProductManager.getProducts());
 // Add product
-myProductManager.addProduct(product1);
+// myProductManager.addProduct(product1);
 // Show current products. Should have one product
-console.log(myProductManager.getProducts());
+// console.log(myProductManager.getProducts());
 // Add product again. Should show error message
-myProductManager.addProduct(product1);
+// myProductManager.addProduct(product1);
 // Get product by id. Should return product1
-console.log(myProductManager.getProductById(0));
+// console.log(myProductManager.getProductById(0));
 // Get product by unexisting id. Should return error message
-console.log(myProductManager.getProductById(1));
-// Create product 2
-const product2 = {
-    title: "producto prueba 2",
-    description: "Este es un producto prueba 2",
-    price: 200,
-    thumbnail: "Sin imagen",
-    code: "abc321",
-    stock: 25,
-};
+// console.log(myProductManager.getProductById(1));
 // Add product 2
-myProductManager.addProduct(product2);
+// myProductManager.addProduct(product2);
 // Show current products. Should have two products
-console.log(myProductManager.getProducts());
+// console.log(myProductManager.getProducts());
 // Get product by previously unexisting id. Now should return product2
-console.log(myProductManager.getProductById(1));
+// console.log(myProductManager.getProductById(1));
--- src/ProductManager.ts
+++ src/ProductManager.ts
@@ -1,4 +1,6 @@
 
+const fs = require("fs");
+
 type Product = {
   id?: number;
   title: string;
@@ -11,13 +13,42 @@ type Product = {
 
 
 class ProductManager {
+  private path: string;
   private products: Product[];
   private currentId: number;
-  constructor() {  
-    this.products = [];
+  constructor(path: string) {  
+    this.path = path;
+    this.products  = [];
     this.currentId = 0;
   }
-  addProduct(product: Product) {
+  async loadData() {
+    try {
+      const data = await fs.promises.readFile(this.path, "utf-8");
+      const parsedData = JSON.parse(data);
+      this.products = parsedData.products as Product[];
+      this.currentId = parsedData.lastId as number;
+      console.log(this.currentId)
+    } catch (error) {
+      console.log("Error loading data!");
+      console.log("Creating new data file...")
+      try {
+        await fs.promises.writeFile(this.path, JSON.stringify({lastId: 0, products: []}, null, 2), 'utf-8');
+        console.log("Data file created");
+      }
+      catch (error) {
+        console.log("Error creating data file!");
+      }
+    }
+  }
+  private async saveData() {
+    try {
+      await fs.promises.writeFile(this.path, JSON.stringify({lastId: this.currentId, products: this.products}, null, 2), 'utf-8');
+    } catch (error) {
+      console.log("Error saving data!");
+    }
+  }
+  
+  async addProduct(product: Product) {
     // Check if product already exists
     if (this.products.some((item) => item.code === product.code)) {
       console.log("Product already exists");
@@ -36,8 +67,9 @@ class ProductManager {
       return;
     }
     // Else, add product
-    product = { id: this.currentId++, ...product };
+    product = { id: ++this.currentId, ...product };
     this.products.push(product);
+    await this.saveData();
   }
 
   getProductById(id: number): Product | string {
@@ -51,54 +83,30 @@ class ProductManager {
 }
 
 // TESTS //
+test();
 
-// Create product manager
-const myProductManager = new ProductManager();
-
-// Show current products. Should be empty
-console.log(myProductManager.getProducts());
-
-// Create product 1
-const product1: Product = {
-  title: "producto prueba",
-  description: "Este es un producto prueba 1",
-  price: 200,
-  thumbnail: "Sin imagen",
-  code: "abc123",
-  stock: 25,
-};
-
-
-// Add product
-myProductManager.addProduct(product1);
-
-// Show current products. Should have one product
-console.log(myProductManager.getProducts());
-
-// Add product again. Should show error message
-myProductManager.addProduct(product1);
-
-// Get product by id. Should return product1
-console.log(myProductManager.getProductById(0));
-
-// Get product by unexisting id. Should return error message
-console.log(myProductManager.getProductById(1));
-
-// Create product 2
-const product2: Product = {
-  title: "producto prueba 2",
-  description: "Este es un producto prueba 2",
-  price: 200,
-  thumbnail: "Sin imagen",
-  code: "abc321",
-  stock: 25,
-};
-
-// Add product 2
-myProductManager.addProduct(product2);
-
-// Show current products. Should have two products
-console.log(myProductManager.getProducts());
-
-// Get product by previously unexisting id. Now should return product2
-console.log(myProductManager.getProductById(1));
+async function test(){
+  // Create product manager instance
+  const myProductManager = new ProductManager("data.json");
+  // Load data.json file if exists. Else, it creates a new data.json file
+  await myProductManager.loadData();
+  // Show current products
+  console.log(myProductManager.getProducts());
+  // Created product1
+  const product1: Product = {
+    title: "producto prueba",
+    description: "Este es un producto prueba 1",
+    price: 200,
+    thumbnail: "Sin imagen",
+    code: "abc123",
+    stock: 25,
+  };
+  // Add product and await to save changes in data file.
+  await myProductManager.addProduct(product1);
+  // Show current products
+  console.log(myProductManager.getProducts());
+  // Check product by ID
+  console.log(myProductManager.getProductById(0));
+  // TODO Update product1
+  // TODO Delete product1
+}
