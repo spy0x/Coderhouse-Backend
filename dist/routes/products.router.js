ce6cbadaef53d95d96bc4cf6268f75c2c6380c7d
Francisco V. Diaz <fevarga1@uc.cl>
2023-05-05 12:55:04 -0400

    handle errors for limit query param done!

--- dist/routes/products.router.js
+++ dist/routes/products.router.js
@@ -6,8 +6,19 @@ productsRouter.get("/", (req, res) => {
     // if countLimit exists, convert it to number, and list products with the specified limit. Else, list all products.
     if (countLimit) {
         const limit = parseInt(countLimit);
-        const result = products.slice(0, limit);
-        return res.json(result);
+        if (isNaN(limit)) {
+            return res.status(400).json({ status: "error", message: "Invalid limit" });
+        }
+        else {
+            if (limit < 1) {
+                return res.status(400).json({ status: "error", message: "Limit must be greater than 0" });
+            }
+            else if (limit > products.length) {
+                return res.status(400).json({ status: "error", message: "Limit must be less than or equal to the number of products" });
+            }
+            const result = products.slice(0, limit);
+            return res.json(result);
+        }
     }
     else {
         return res.json(products);
--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -4,26 +4,36 @@ import { products } from "../App.js";
 const productsRouter = Router();
 
 productsRouter.get("/", (req, res) => {
-    const countLimit = req.query.limit;
+  const countLimit = req.query.limit;
 
-    // if countLimit exists, convert it to number, and list products with the specified limit. Else, list all products.
-    if (countLimit) {
-      const limit = parseInt(countLimit as string);
+  // if countLimit exists, convert it to number, and list products with the specified limit. Else, list all products.
+  if (countLimit) {
+    const limit = parseInt(countLimit as string);
+    if (isNaN(limit)) {
+      return res.status(400).json({ status: "error", message: "Invalid limit" });
+    } else {
+      if (limit < 1) {
+        return res.status(400).json({ status: "error", message: "Limit must be greater than 0" });
+      }
+      else if (limit > products.length) { 
+        return res.status(400).json({ status: "error", message: "Limit must be less than or equal to the number of products" });  
+      }
       const result = products.slice(0, limit);
       return res.json(result);
-    } else {
-      return res.json(products);
     }
-  });
-  productsRouter.get("/:pid", (req, res) => {
-    const id = parseInt(req.params.pid);
-    let product : Product | undefined = undefined;
-    if(typeof products === "object")
-    {
-      product = products.find((item) => item.id === id);
-    }
-    const result = product ? product : "Product not found";
-    res.json(result);
-  });
+  } else {
+    return res.json(products);
+  }
+});
+
+productsRouter.get("/:pid", (req, res) => {
+  const id = parseInt(req.params.pid);
+  let product: Product | undefined = undefined;
+  if (typeof products === "object") {
+    product = products.find((item) => item.id === id);
+  }
+  const result = product ? product : "Product not found";
+  res.json(result);
+});
 
 export default productsRouter;
