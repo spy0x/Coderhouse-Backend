bdb14183b62cdbda010de33bf37fbeca3058b31e
Francisco V. Diaz <fevarga1@uc.cl>
2023-06-03 12:52:46 -0400

    old productmanager with filesystem recovered but not used

--- src/dao/ProductManager.ts
+++ src/dao/ProductManager.ts
@@ -1,6 +1,5 @@
 import fs from "fs";
-import { ProductModel } from "./models/products.models.js";
-// import { ProductModel } from "./models/products.models.js";
+
 export default class ProductManager {
   private path: string;
   private products: Product[];
@@ -12,155 +11,138 @@ export default class ProductManager {
     this.currentId = 0;
   }
 
-  // async loadData() {
-  //   try {
-  //     const data = await fs.promises.readFile(this.path, "utf-8");
-  //     const parsedData = JSON.parse(data);
-  //     this.products = parsedData.products as Product[];
-  //     this.currentId = parsedData.lastId as number;
-  //   } catch (error) {
-  //     console.log("Error loading data!");
-  //     console.log("Creating new data file...");
-  //     try {
-  //       await fs.promises.writeFile(this.path, JSON.stringify({ lastId: 0, products: [] }, null, 2), "utf-8");
-  //       await this.loadData();
-  //       console.log("Data file created");
-  //     } catch (error) {
-  //       console.log("Error creating data file!");
-  //     }
-  //   }
-  // }
-
-  // private async saveData() {
-  //   try {
-  //     await fs.promises.writeFile(
-  //       this.path,
-  //       JSON.stringify({ lastId: this.currentId, products: this.products }, null, 2),
-  //       "utf-8"
-  //     );
-  //   } catch (error) {
-  //     console.log("Error saving data!");
-  //   }
-  // }
-
-  async addProduct(product: Product): Promise<ResResult> {
+  async loadData() {
     try {
-      // Check if product already exists
-      if (await ProductModel.findOne({ code: product.code })) {
-        return { code: 400, result: { status: "error", message: "Product already exists" } };
-      }
-      // Check if product has all required properties
-      if (
-        !product.title ||
-        !product.description ||
-        !product.price ||
-        !product.code ||
-        !product.category ||
-        !product.stock
-      ) {
-        return { code: 400, result: { status: "error", message: "Product is missing required properties" } };
-      }
-      // If no thumbnail, set default value to empty array
-      if (!product.thumbnail) {
-        product.thumbnail = [];
-      }
-      // if no status, set default value to true
-      if (!product.status) {
-        product.status = true;
-      }
-      // Add product
-      ProductModel.create(product);
-      return { code: 201, result: { status: "success", message: "Product added successfully", payload: product } };
+      const data = await fs.promises.readFile(this.path, "utf-8");
+      const parsedData = JSON.parse(data);
+      this.products = parsedData.products as Product[];
+      this.currentId = parsedData.lastId;
     } catch (error) {
-      return { code: 400, result: { status: "error", message: "Error adding product" } };
+      console.log("Error loading data!");
+      console.log("Creating new data file...");
+      try {
+        await fs.promises.writeFile(this.path, JSON.stringify({ lastId: 0, products: [] }, null, 2), "utf-8");
+        await this.loadData();
+        console.log("Data file created");
+      } catch (error) {
+        console.log("Error creating data file!");
+      }
     }
   }
 
-  async getProductById(id: string) {
+  private async saveData() {
     try {
-      const product = await ProductModel.find({ _id: id });
-      if (product) {
-        return { code: 200, result: { status: "success", payload: product } };
-      } else {
-        return { code: 404, result: { status: "error", message: "Product not found" } };
-      }
+      await fs.promises.writeFile(
+        this.path,
+        JSON.stringify({ lastId: this.currentId, products: this.products }, null, 2),
+        "utf-8"
+      );
     } catch (error) {
+      console.log("Error saving data!");
+    }
+  }
+
+  async addProduct(product: Product): Promise<ResResult> {
+    // Check if product already exists
+    if (this.products.some((item) => item.code === product.code)) {
+      return { code: 400, result: { status: "error", message: "Product already exists" } };
+    }
+    // Check if product has all required properties
+    if (
+      !product.title ||
+      !product.description ||
+      !product.price ||
+      !product.code ||
+      !product.category ||
+      !product.stock
+    ) {
+      return { code: 400, result: { status: "error", message: "Product is missing required properties" } };
+    }
+    // If no thumbnail, set default value to empty array
+    if (!product.thumbnail) {
+      product.thumbnail = [];
+    }
+    // if no status, set default value to true
+    if (!product.status) {
+      product.status = true;
+    }
+    // Add product
+    const newId = ++this.currentId;
+    product = { _id: newId.toString(), ...product };
+    this.products.push(product);
+    await this.saveData();
+    return { code: 201, result: { status: "success", message: "Product added successfully", payload: product } };
+  }
+
+  getProductById(id: string): ResResult {
+    const product = this.products.find((item) => item._id === id);
+    if (product) {
+      return { code: 200, result: { status: "success", payload: product } };
+    } else {
       return { code: 404, result: { status: "error", message: "Product not found" } };
     }
   }
 
-  async getProducts(countLimit: any): Promise<ResResult> {
-    try {
-      const products = await ProductModel.find().lean().exec();
-      // if products array is empty, return error
-      if (products.length === 0) {
-        return { code: 404, result: { status: "error", message: "No products found." } };
-      }
-      // if countLimit exists, list products with the specified limit. Else, list all products.
-      if (countLimit) {
-        if (isNaN(countLimit)) {
-          return { code: 400, result: { status: "error", message: "Invalid limit" } };
-        } else if (countLimit < 1) {
-          return { code: 400, result: { status: "error", message: "Limit must be greater than 0" } };
-        } else {
-          try {
-            const products = await ProductModel.find().limit(countLimit);
-            return { code: 200, result: { status: "success", payload: products } };
-          } catch (error) {
-            return { code: 400, result: { status: "error", message: "Error getting products" } };
-          }
-        }
+  getProducts(countLimit: any): ResResult {
+    // if countLimit exists, convert it to number, and list products with the specified limit. Else, list all products.
+    if (countLimit) {
+      if (isNaN(countLimit)) {
+        return { code: 400, result: { status: "error", message: "Invalid limit" } };
       } else {
-        return { code: 200, result: { status: "success", payload: products } };
+        if (countLimit < 1) {
+          return { code: 400, result: { status: "error", message: "Limit must be greater than 0" } };
+        } else if (countLimit > this.products.length) {
+          return {
+            code: 400,
+            result: { status: "error", message: "Limit must be less than or equal to the number of products" },
+          };
+        }
+        const result = this.products.slice(0, countLimit);
+        return { code: 200, result: { status: "success", payload: result } };
       }
-    } catch (error) {
-      return { code: 400, result: { status: "error", message: "Error getting products" } };
+    } else {
+      return this.products.length
+        ? { code: 200, result: { status: "success", payload: this.products } }
+        : { code: 404, result: { status: "error", message: "No products found." } };
     }
   }
 
   // Update one or more properties of a product id
-  async updateProduct(id: string, productAttributes: ProductKeys): Promise<ResResult> {
-    try {
-      // Check if product exists
-      const product = await ProductModel.find({ _id: id });
-      if (!product) {
-        return { code: 404, result: { status: "error", message: "Product not found" } };
-      }
-      // Else, update product and secure id property is not modified
-      await ProductModel.updateOne({ _id: id }, productAttributes);
-      return {
-        code: 200,
-        result: {
-          status: "success",
-          message: "Product updated successfully",
-          payload: await ProductModel.findOne({ _id: id }),
-        },
-      };
-    } catch (error) {
-      return { code: 400, result: { status: "error", message: "Error updating product" } };
+  async updateProduct(id: string, product: ProductKeys): Promise<ResResult> {
+    // Check if product exists
+    const productIndex = this.products.findIndex((product) => product._id === id);
+    if (productIndex === -1) {
+      return { code: 404, result: { status: "error", message: "Product not found" } };
     }
+    // Else, update product and secure id property is not modified
+    this.products[productIndex] = { ...this.products[productIndex], ...product, _id: this.products[productIndex]._id };
+    await this.saveData();
+    return {
+      code: 200,
+      result: {
+        status: "success",
+        message: "Product updated successfully",
+        payload: this.products[productIndex],
+      },
+    };
   }
 
   async deleteProduct(id: string): Promise<ResResult> {
-    try {
-      // Check if product exists
-      const product = await ProductModel.find({ _id: id });
-      if (!product) {
-        return { code: 404, result: { status: "error", message: "Product not found" } };
-      }
-      // Else, delete product
-      await ProductModel.deleteOne({ _id: id });
-      return {
-        code: 200,
-        result: { status: "success", message: "Product deleted successfully", payload: product },
-      };
-    } catch {
-      return { code: 400, result: { status: "error", message: "Error deleting product" } };
+    // Check if product exists
+    const productIndex = this.products.findIndex((product) => product._id === id);
+    if (productIndex === -1) {
+      return { code: 404, result: { status: "error", message: "Product not found" } };
     }
+    // Else, delete product
+    const deletedProduct = this.products.splice(productIndex, 1);
+    await this.saveData();
+    return {
+      code: 200,
+      result: { status: "success", message: "Product deleted successfully", payload: deletedProduct },
+    };
   }
 
-  async productExists(id: number) {
-
-    return await ProductModel.findOne({ _id: id });
+  productExists(id: string) {
+    return this.products.some((item) => item._id === id);
   }
 }
