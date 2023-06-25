4cbf15ac4b2dce58992597057a097c0bf2f0b8fd
Francisco V. Diaz <fevarga1@uc.cl>
2023-06-25 19:22:00 -0400

    encrypted passwords done!

--- dist/services/users.services.js
+++ dist/services/users.services.js
@@ -1,8 +1,9 @@
 import { UserModel } from "../models/users.models.js";
+import { createHash, compareHash } from "../utils/passwordCrypt.js";
 export default class UserService {
     async addUser(first_name, last_name, email, age, password, role = "user") {
         try {
-            const user = await UserModel.create({ first_name, last_name, email, age, password, role });
+            const user = await UserModel.create({ first_name, last_name, email, age, password: createHash(password), role });
             return { code: 201, result: { status: "success", message: "User created successfully", payload: user } };
         }
         catch (error) {
@@ -11,12 +12,15 @@ export default class UserService {
     }
     async login(email, password) {
         try {
-            const account = await UserModel.findOne({ email, password });
+            const account = await UserModel.findOne({ email });
             if (account) {
+                if (!compareHash(password, account.password)) {
+                    return { code: 404, result: { status: "error", message: "Wrong user or password!" } };
+                }
                 return { code: 200, result: { status: "success", message: "User logged in successfully", payload: account } };
             }
             else {
-                return { code: 404, result: { status: "error", message: "Wrong user or password!" } };
+                return { code: 404, result: { status: "error", message: "Account does not exists!" } };
             }
         }
         catch (error) {
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,4 +1,5 @@
 import { UserModel } from "../models/users.models.js";
+import { createHash, compareHash } from "../utils/passwordCrypt.js";
 
 export default class UserService {
   async addUser(
@@ -10,7 +11,7 @@ export default class UserService {
     role: string = "user"
   ): Promise<ResResult> {
     try {
-      const user = await UserModel.create({ first_name, last_name, email, age, password, role });
+      const user = await UserModel.create({ first_name, last_name, email, age, password: createHash(password), role });
       return { code: 201, result: { status: "success", message: "User created successfully", payload: user } };
     } catch (error) {
       return { code: 400, result: { status: "error", message: "Error adding user" } };
@@ -18,11 +19,14 @@ export default class UserService {
   }
   async login(email: string, password: string): Promise<ResResult> {
     try {
-      const account = await UserModel.findOne({ email, password });
+      const account = await UserModel.findOne({ email });
       if (account) {
+        if (!compareHash(password, account.password)) {
+          return { code: 404, result: { status: "error", message: "Wrong user or password!" } };
+        }
         return { code: 200, result: { status: "success", message: "User logged in successfully", payload: account } };
       } else {
-        return { code: 404, result: { status: "error", message: "Wrong user or password!" } };
+        return { code: 404, result: { status: "error", message: "Account does not exists!" } };
       }
     } catch (error) {
       return { code: 400, result: { status: "error", message: "Error logging in" } };
