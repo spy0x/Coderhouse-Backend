8eec4c40fdfd38d9069fe1f45417a5c9ee82a7eb
Francisco V. Diaz <fevarga1@uc.cl>
2023-08-11 20:53:20 -0400

    installed react dom and material ui. made first sample page and navbar

--- frontend_react/src/App.tsx
+++ frontend_react/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Roboto,sans-serif",
+    },
+    palette: {
+      mode: "dark",
+      background: {
+        default: "#121212",
+      },
+    },
+  });
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </CssBaseline>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
