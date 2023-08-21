59b48fe5e68d7d169c3eadbe004b5b4b465179a9
Francisco V. Diaz <fevarga1@uc.cl>
2023-08-20 21:32:00 -0400

    implemented products query params

--- frontend_react/src/pages/Products.tsx
+++ frontend_react/src/pages/Products.tsx
@@ -1,18 +1,26 @@
 import { CircularProgress, Container, Stack, Typography, Grid } from "@mui/material";
 import { useLayoutEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
+import { useLocation } from "react-router-dom";
 
 export default function Products() {
   const [products, setProducts] = useState<Product[]>([]);
   const [loadingProducts, setLoadingProducts] = useState(false);
+  const urlParams = new URLSearchParams(useLocation().search);
+  const limit = urlParams.get("limit") || 10;
+  const page = urlParams.get("page") || 1;
+  const query = urlParams.get("query") ? `&query=${urlParams.get("query")}` : "";
+  const sort = urlParams.get("sort") ? `&sort=${urlParams.get("sort")}` : "";
+
+
   const getProducts = async () => {
     setLoadingProducts(true);
     const apiUrl = import.meta.env.VITE_URL;
-    const response = await fetch(`${apiUrl}/api/products`);
+    const response = await fetch(`${apiUrl}/api/products?limit=${limit}&page=${page}${sort}${query}`);
     const data = await response.json();
     setLoadingProducts(false);
     setProducts(data.payload);
-    console.log(data.payload);
+    console.log(data);
   };
 
   useLayoutEffect(() => {
